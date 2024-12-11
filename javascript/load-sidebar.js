dbb5e8d96bf01ab8a5a320c5d154d43e39f1dceb
GhidBase <grvdylans@gmail.com>
2024-12-11 13:20:08 -0700

    Adjust sidebar loader

--- javascript/load-sidebar.js
+++ javascript/load-sidebar.js
@@ -1,5 +1,13 @@
 const htmlFilePath = "sidebar.html";
 fetch(htmlFilePath)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text(); // Extract the HTML content as a string
+    })
     .then(html => {
         document.querySelector(".side-bar").innerHTML = html;
     })
+
+console.log("Sidebar loaded!")
