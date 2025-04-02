e3a9598ddab897a0aa7520703492d83224db1854
Dylan Simon <grvdylans@gmail.com>
2025-04-02 00:01:03 -0600

    Bug fix for background scrolling

--- javascript/open-navigation.js
+++ javascript/open-navigation.js
@@ -1,14 +1,17 @@
 const navButton = document.querySelector(".menu");
 const navMenu = document.querySelector(".side-bar");
 const cover = document.querySelector(".cover");
+const body = document.querySelector("body");
 
 navButton.addEventListener("click", () => {
     console.log("hi");
     navMenu.classList.toggle("visible");
     cover.classList.toggle("visible");
+    body.classList.toggle("unscrollable");
 })
 
 cover.addEventListener("click", () => {
     navMenu.classList.toggle("visible");
     cover.classList.toggle("visible");
+    body.classList.toggle("unscrollable");
 })
