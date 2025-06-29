0525411723d308713f0b50739470931dec176355
Dylan Simon <grvdylans@gmail.com>
2025-06-29 14:09:21 -0600

    Add new js to immortal guide

--- javascript/tab-controller-two.js
+++ javascript/tab-controller-two.js
@@ -4,13 +4,7 @@ tabList.forEach((tab) => {
     const tabBody = tab.querySelector(".toggle-panel");
     button.addEventListener("click", () => {
         // If the panel is already expanded
-        if (
-            tabBody.style.maxHeight ||
-            tabBody.classList.contains("expand-details")
-        ) {
-            if (tabBody.id == "details-panel") {
-                tabBody.classList.remove("expand-details");
-            }
+        if (tabBody.style.maxHeight) {
             tabBody.style.maxHeight = null;
             tabBody.classList.add("unscrollable");
             tabBody.addEventListener(
@@ -25,26 +19,15 @@ tabList.forEach((tab) => {
                 { once: true }
             );
         } else {
-            // If the panel is collapsed
-            tabBody.classList.add("unscrollable");
+            tabBody.classList.remove("collapsed");
             tabBody.addEventListener("transitionend", () => {
                 tabBody.classList.remove("unscrollable");
             });
             if (tabBody.id == "roulette-log") {
                 tabBody.style.maxHeight = "300px";
-            } else if (tabBody.id == "details-panel") {
-                const isSmallScreen = window.matchMedia(
-                    "(max-width: 1000px)"
-                ).matches;
-                if (isSmallScreen) {
-                    tabBody.classList.add("expand-details");
-                } else {
-                    tabBody.style.maxHeight = tabBody.scrollHeight + "px";
-                }
             } else {
                 tabBody.style.maxHeight = tabBody.scrollHeight + "px";
             }
-            tabBody.classList.remove("collapsed");
             button.classList.add("expanded-button");
         }
     });
